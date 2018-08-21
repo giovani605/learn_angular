24839a40ba0a66a93a84c7c14bec46a72645755b
Giovani <giovanibossoni@hotmail.com>
2018-08-21 14:46:37 -0300

    componentes funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { NovoCompComponent } from './novo-comp/novo-comp.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    NovoCompComponent
+    NovoCompComponent,
+    TestCompComponent
   ],
   imports: [
     BrowserModule
